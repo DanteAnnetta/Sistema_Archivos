620bd5698331ee376ea9714a8c3ea440bed1e44c
DanteAnnetta <65869218+DanteAnnetta@users.noreply.github.com>
2023-02-08 16:20:38 -0300

    Add files via upload

--- arch.hpp
+++ arch.hpp
@@ -4,7 +4,8 @@
 #include <iostream>
 #include "listascomp.hpp"
 #include "listasordc.hpp"
-#include "disk.hpp"
+#include "rwstring.hpp"
+
 using namespace std;
 
 
@@ -40,6 +41,160 @@ int criterio_carpeta(Dir a , Dir b){
 int criterio_arch(File a , File b){
     return a.id.compare(b.id);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+const int lline = 180;  // cantidad máxima de caracteres definida por cada linea
+char lin[lline];        // esto podría moverse a la función de escritura
+
+struct Line{
+    string cont;
+};
+
+ostream& operator << (ostream & os , Line l){
+    os << l.cont;
+    return os;
+}
+// lectura
+fstream& operator >> (fstream& fs , Line& l){
+    l.cont = readstring(fs , lline);
+    return fs;
+}
+// escritura
+fstream& operator << (fstream& fs , Line l){
+    writestring(fs , l.cont , lline);
+    return fs;
+}
+
+
+
+
+void mostrar_archivo (Dir* curs, string clav , Nodo<File>* lista){   // función para leer el contenido de un archivo
+    Line aux;
+    File mf;
+    mf.id = clav;
+    Nodo<File>* f;
+    f = buscar<File>(mf , curs->archs , criterio_arch);
+    fstream file1;
+    file1.open("disk.bin" , ios::binary | ios::in);
+    if(!file1){
+        cout << "Error: no se pudo abrir el archivo" << endl;
+    }
+    int i = 0;
+    while(file1 >> aux){
+        if (i -1 > f->dato.ini && i -1 < f->dato.fin){  
+            cout << aux.cont << endl;
+        }
+        i ++;
+    }
+    file1.close();
+
+}
+
+void crear_file(File & f){
+    Line aux;
+    fstream file1;
+    int ini = 0;
+    int fin;
+    file1.open("disk.bin" , ios::binary | ios::out);
+    while(file1 >> aux){
+        ini++;
+    }
+    f.ini = ini; // se registra a partir de que linea comienza el archivo
+    fin = ini;
+    file1.close();
+    
+    file1.open("disk.bin" , ios::binary | ios::app);
+
+    string dat = "";
+    int q = 0;
+    //while (dat!=":q"){ 
+    while (q < 10){   // este método no funciona
+        cout << "/" ;
+        cin.getline(lin , lline , '\n');
+        for(int i = 0; i< lline ;i++){
+            dat += lin[i];
+        }
+        aux.cont = dat;
+        file1 << aux;
+        aux.cont = "";
+        dat = "";
+
+        fin++;
+        q++;
+    }
+    f.fin = fin;   // el archivo queda definido en su extensión y queda agregado al "disco"
+    file1.close();
+
+}
+
+void eliminar_archivo( Dir*curs, string clav ,  Nodo<File>* lista){
+    Line aux;
+    File mf;
+    mf.id = clav;
+    Nodo<File>* f;
+    Nodo<File>* p = lista;
+    f = buscar<File>(mf , curs->archs , criterio_arch);
+    int i;
+    int in;
+    int j;
+    fstream file1;
+    fstream file2;
+
+
+
+    file2.open("disk2.bin", ios::binary | ios::in);
+    while(p->sig){  // se buscan los archivos de la lista uno por uno
+        if (p->dato.ini == f->dato.ini && p->dato.fin == f->dato.fin){ // si el archivo a eliminar es igual al de la lista lo pasa
+            continue;
+        }
+        else{
+            file1.open("disk.bin" , ios::binary | ios::out);
+            j = 0;
+            in = i;
+            while(file1>>aux){
+                if (j -1 > p->dato.ini && j -1 < p->dato.fin){  
+                    //cout << aux.cont << endl;
+                    file2 << aux; // se escribe el dato en el segundo archivo
+                    i++;
+                }
+                j ++;
+            }
+            p->dato.ini = in;
+            p->dato.fin = i;
+            file1.close();
+        }
+    }
+    
+    remove("disk.bin");
+    //rename("disk2.bin" , "disk.bin");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -145,6 +300,18 @@ void cd (string clav , Dir* &curs , string &path){
     }
     
 }
+
+//----------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 
 
 
